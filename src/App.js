46f021029ff6d399b2d9d2e3642cbdec781a47e2
guiqsassi <guiqsassi@gmail.com>
2023-05-23 13:55:56 -0300

    primeira seção

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Lorax from "./images/Lorax.png"
+import Nav from './component/Nav';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='container'>
+        <Nav></Nav>
+        <section className='landing'>
+            <div className='LoraxImage'>
+                  <img className='LoraxPng' src={Lorax}/> 
+            </div>
+            <div className='LandingText'> 
+                <h1 className='title'>
+                  Lorax
+                </h1>
+                <p>
+                Lorem ipsum dolor sit amet. Et suscipit enim aut eligendi explicabo sit dolorem debitis et odit ipsam et voluptatem minus. Ea placeat rerum ut dolore nostrum <br/>aut repudiandae accusantium. Aut modi neque a accusantium nemo aut quibusdam tenetur et repellat voluptatem ut repudiandae corporis. Non laudantium asperiores<br/> quo alias galisum qui rerum libero aut repellendus mollitia.
+                </p>
+            </div>
+        </section>
+        
+        <section className='about'>
+
+        </section>
+        
+        <section className='contact'>
+
+        </section>
     </div>
   );
 }
---
+++ src/component/Nav.js
@@ -0,0 +1,24 @@
+import './Nav.css';
+
+
+function Nav(){
+    return(
+    <div className="Container">
+    <nav>
+        <ul>
+            <li>
+                <a>Home</a>
+            </li>
+            <li>
+                <a>About</a>
+            </li>
+            <li>
+                <a>Contact</a>
+            </li>
+        </ul>
+    </nav>
+    </div>
+    );
+
+}
+export default Nav
