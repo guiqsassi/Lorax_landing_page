509179d0b8082934aebfeca68d02c45f8901970d
guiqsassi <guiqsassi@gmail.com>
2023-05-25 10:47:30 -0300

    Logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import Lorax from "./images/Lorax.png"
+import '../src/cssFiles/App.css';
+import '../src/cssFiles/About.css';
+import Lorax from "./images/Lorax.png";
 import Nav from './component/Nav';
 import Logo from "./images/NicePng_lorax-png_581113.png"
 function App() {
   return (
     
     <div>
+      <Nav></Nav>
         <section className='landing'>
-        <Nav></Nav>
+        
             <div className='LandingContainer'>
             <div className='Left'> 
             
              
-             <div className="Description">    
-                <img className="Logo" src={Logo}/>
-                <p>O menino Ted (Zac Efron) descobriu que o sonho de sua paixão, a bela Audrey (Taylor Swift), é ver uma árvore de verdade, algo em extinção. Disposto a realizar este desejo, ele embarca numa aventura por uma terra desconhecida, cheia de cor, natureza e árvores. É lá que conhece também o simpático e ao mesmo tempo rabugento Lorax (Danny DeVito), uma criatura curiosa preocupada com o futuro de seu próprio mundo. 
+             <div className="Description">   
+                <div className='logoDiv'>
+                  <img className="Logo" src={Logo}/>
+                </div> 
+                
+                <p>O menino Ted descobriu que o sonho de sua paixão, a bela Audrey, é ver uma árvore de verdade, algo em extinção. Disposto a realizar este desejo, ele embarca numa aventura por uma terra desconhecida, cheia de cor, natureza e árvores. É lá que conhece também o simpático e ao mesmo tempo rabugento Lorax, uma criatura curiosa preocupada com o futuro de seu próprio mundo. 
                 </p>
                 </div>
                 <div className="buttons">
@@ -27,14 +32,7 @@ function App() {
             </div>
             </div>
         </section>
-        
-        <section className='about'>
 
-        </section>
-        
-        <section className='contact'>
-
-        </section>
     </div>
   );
 }
