b34963161c747135d70ad580f936b749bea15055
guiqsassi <guiqsassi@gmail.com>
2023-05-25 02:12:03 -0300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 import Lorax from "./images/Lorax.png"
 import Nav from './component/Nav';
+import Logo from "./images/NicePng_lorax-png_581113.png"
 function App() {
   return (
     
-    <div className='container'>
-        <Nav></Nav>
+    <div>
         <section className='landing'>
+        <Nav></Nav>
+            <div className='LandingContainer'>
+            <div className='Left'> 
+            
+             
+             <div className="Description">    
+                <img className="Logo" src={Logo}/>
+                <p>O menino Ted (Zac Efron) descobriu que o sonho de sua paixão, a bela Audrey (Taylor Swift), é ver uma árvore de verdade, algo em extinção. Disposto a realizar este desejo, ele embarca numa aventura por uma terra desconhecida, cheia de cor, natureza e árvores. É lá que conhece também o simpático e ao mesmo tempo rabugento Lorax (Danny DeVito), uma criatura curiosa preocupada com o futuro de seu próprio mundo. 
+                </p>
+                </div>
+                <div className="buttons">
+                    <button className="Trailer">Watch Trailer</button>
+                    <button className="Watch">Watch full</button>
+                </div>
+            </div>
             <div className='LoraxImage'>
                   <img className='LoraxPng' src={Lorax}/> 
             </div>
-            <div className='LandingText'> 
-                <h1 className='title'>
-                  Lorax
-                </h1>
-                <p>
-                Lorem ipsum dolor sit amet. Et suscipit enim aut eligendi explicabo sit dolorem debitis et odit ipsam et voluptatem minus. Ea placeat rerum ut dolore nostrum <br/>aut repudiandae accusantium. Aut modi neque a accusantium nemo aut quibusdam tenetur et repellat voluptatem ut repudiandae corporis. Non laudantium asperiores<br/> quo alias galisum qui rerum libero aut repellendus mollitia.
-                </p>
             </div>
         </section>
         
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,13 +1,15 @@
 import './Nav.css';
-
-
+import Logo from "../images/kisspng-the-lorax-once-ler-youtube-clip-art-dr-seuss-5ac9bfec3bdba8.9696196215231713082452.png"
+import {FaFacebookSquare, FaInstagram, FaTiktok, FaGithub} from "react-icons/fa";
 function Nav(){
     return(
     <div className="Container">
     <nav>
-        <ul>
+        <div className="mainNavegation"> 
+            <img src={Logo} className="LogoNav"/>
+            <ul>
             <li>
-                <a>Home</a>
+                <a  src={"https://www.twitch.tv/ranboolive"}>Home</a>
             </li>
             <li>
                 <a>About</a>
@@ -16,6 +18,13 @@ function Nav(){
                 <a>Contact</a>
             </li>
         </ul>
+        </div>
+        <div className="socialMediaNavegation">
+            <FaFacebookSquare size={25}  href="https://google.com" target="_blank" className='socialIcon'></FaFacebookSquare>
+            <FaInstagram size={25} href="https://www.twitch.tv/ranboolive" target="_blank" className='socialIcon'></FaInstagram>
+            <FaTiktok size={25} className='socialIcon'></FaTiktok>
+            <FaGithub size={25} className='socialIcon'></FaGithub>  
+        </div>
     </nav>
     </div>
     );
